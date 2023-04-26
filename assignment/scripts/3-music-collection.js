72d1fe67cd5659f6016e039d0811caf3e31cfc45
Dave Nash <davidjnash2@gmail.com>
2023-04-26 09:03:47 -0500

    addToCollection complete and seems to be working

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,24 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+
+function addToCollection( title, artist, yearPublished ) {
+    let album= {
+    theTitle: title,
+    theArtist: artist,
+    theYearPublished: yearPublished
+    }
+    collection.push(album);
+    return album
+} // end addToCollection
+
+console.log(addToCollection('Ready to Die', 'Notorious BIG', '1994'));
+console.log(collection);
+
+
+addToCollection('This is a Long Drive for Someone with Nothing to Think About', 'Modest Mouse', '1996');
+addToCollection('The Velvet Underground & Nico', 'The Velvet Underground', '1967');
+addToCollection('Loveless', 'My Bloody Valentine', '1991');
+addToCollection('Marquee Moon', 'Television', '1977');
+addToCollection('The Modern Lovers', 'The Modern Loves', '1976');
+addToCollection('Abbatoir Blues/The Lyre of Orpheus', 'Nick Cave & The Bad Seeds', '2004');
