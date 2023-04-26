31c1480f03089e4ffe80f17a79ccb419765100b4
Dave Nash <davidjnash2@gmail.com>
2023-04-26 11:03:00 -0500

    ok, think I got AddToCollection working for real this time

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,23 +2,34 @@ console.log('***** Music Collection *****')
 
 let collection = [];
 
-function addToCollection( title, artist, yearPublished ) {
-    let album= {
-    theTitle: title,
-    theArtist: artist,
-    theYearPublished: yearPublished
+function AddToCollection(title, artist, yearPublished) {
+    const album = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished
     }
     collection.push(album);
-    return album
-} // end addToCollection
+    return album;
+}
 
-console.log(addToCollection('Ready to Die', 'Notorious BIG', '1994'));
+let ReadyToDie = new AddToCollection('Ready to Die', 'Notorious BIG', '1994');
+console.log(ReadyToDie);
+
+let LongDrive = new AddToCollection('This is a Long Drive for Someone with Nothing to Think About', 'Modest Mouse', '1996');
+console.log(LongDrive);
+
+let Banana = new AddToCollection('The Velvet Underground & Nico', 'The Velvet Underground', '1967');
+console.log(Banana);
+
+let Loveless = new AddToCollection('Loveless', 'My Bloody Valentine', '1991');
+console.log(Loveless);
+
+let MarqueeMoon = new AddToCollection('Marquee Moon', 'Television', '1977');
+console.log(MarqueeMoon);
+
+let ModernLovers = new AddToCollection('The Modern Lovers', 'The Modern Lovers', '1976');
+console.log(ModernLovers);
+
+let Abbatoir = new AddToCollection('Abbatoir Blues/The Lyre of Orpheus', 'Nick Cave & The Bad Seeds', '2004');
+console.log(Abbatoir);
 console.log(collection);
-
-
-addToCollection('This is a Long Drive for Someone with Nothing to Think About', 'Modest Mouse', '1996');
-addToCollection('The Velvet Underground & Nico', 'The Velvet Underground', '1967');
-addToCollection('Loveless', 'My Bloody Valentine', '1991');
-addToCollection('Marquee Moon', 'Television', '1977');
-addToCollection('The Modern Lovers', 'The Modern Loves', '1976');
-addToCollection('Abbatoir Blues/The Lyre of Orpheus', 'Nick Cave & The Bad Seeds', '2004');
